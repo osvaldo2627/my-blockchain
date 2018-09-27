0888ef5f442415c248c4b2e346b4595e32c2f6b8
Osvaldo Aguilar <osvaldo2627@gmail.com>
2018-09-27 13:00:33 +0200

    initial commit

---
+++ dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389,'OINDDFFDASDFASDFASDFA', 'D8795DASDFASDFASDFA' );
+bitcoin.createNewBlock(125,'KIDSDDFFDASDFASDFASDFA', 'ADFE95DASDFASDFASDFA' );
+bitcoin.createNewBlock(65987,'DFGDFGDFGDASDFAASDFSSF', 'ASDFASFDSADASDFASDFASDFA' );
+
+console.log(bitcoin);
