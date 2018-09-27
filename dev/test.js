4dde424a19471fbf4b47f18a18d5a61ef57e9971
Osvaldo Aguilar <osvaldo2627@gmail.com>
2018-09-27 13:41:50 +0200

    removing manual test

--- dev/test.js
+++
@@ -1,8 +0,0 @@
-const Blockchain = require('./blockchain');
-
-const bitcoin = new Blockchain();
-
-bitcoin.createNewBlock(2389,'OINDDFFDASDFASDFASDFA', 'D8795DASDFASDFASDFA' );
-bitcoin.createNewBlock(65987,'DFGDFGDFGDASDFAASDFSSF', 'ASDFASFDSADASDFASDFASDFA' );
-
-console.log(bitcoin);
