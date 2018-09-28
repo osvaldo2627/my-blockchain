7aded9d33fe9b12d71b2d679b29aae581c7d0aac
Osvaldo Aguilar <osvaldo2627@gmail.com>
2018-09-29 00:10:46 +0200

    WIP, createNewTransaction and functional test implemented

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,23 +1,56 @@
 
 function Blockchain(){
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
+/**
+ * 
+ * @param {*} nonce 
+ * @param {*} previousBlockHash 
+ * @param {*} hash 
+ */
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash ){
+    
     const newBlock = {
         index : this.chain.length+1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     }
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
+}
+
+/**
+ * 
+ */
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length - 1];
+}
+
+/**
+ * 
+ * @param {*} amount 
+ * @param {*} sender 
+ * @param {*} recepient 
+ */
+Blockchain.prototype.createNewTransaction = function( amount, sender, recepient ){
+
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recepient: recepient
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
 }
 
 module.exports = Blockchain;
--- test/blockchainTest.js
+++ test/blockchainTest.js
@@ -1,15 +1,50 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const  Blockchain = require('../dev/blockchain');
 
 const myNewCoin = new Blockchain();
 
+
 describe('Blockchain createNewBlock', ()=>{
-    it('should return a new block', ()=>{
+    it('should return a new block with the gived hash', ()=>{
 
         let block1 = myNewCoin.createNewBlock(2389,'OINDDFFDASDFASDFASDFA', 'D8795DASDFASDFASDFA' );
-        let block2 = myNewCoin.createNewBlock(65987,'DFGDFGDFGDASDFAASDFSSF', 'ASDFASFDSADASDFASDFASDFA' );
-
+        
         assert.equal(block1.hash, "D8795DASDFASDFASDFA" );
         assert.equal(block1.previousBlockHash, "OINDDFFDASDFASDFASDFA" );
+    })
+
+    it('should add a second block and add it to the chain', ()=>{
+        let block2 = myNewCoin.createNewBlock(65987,'DFGDFGDFGDASDFAASDFSSF', 'ASDFASFDSADASDFASDFASDFA' );
+        
+        assert.equal(block2.hash, "ASDFASFDSADASDFASDFASDFA" );
+        assert.equal(block2.previousBlockHash, "DFGDFGDFGDASDFAASDFSSF" );
+
+        assert.equal(myNewCoin.chain.length, 2);
+    })
+})
+
+
+describe('Blockchain getLastBlock', ()=>{
+    it('should return the last block added', ()=>{
+        let lastBlock = myNewCoin.getLastBlock();
+        assert.equal(lastBlock.index, myNewCoin.chain[myNewCoin.chain.length-1].index );       
+    })
+})
+
+
+describe('Blockchain createNewTransaction', ()=>{
+    it('should create a new transaction and add it to pending transactions', ()=>{
+        myNewCoin.createNewTransaction(100, 'OSV1988303DDF', 'SUS8383JDKDHDH');
+        expect(myNewCoin).to.have.property('pendingTransactions').with.length(1);
+        assert.equal(myNewCoin.pendingTransactions[0].amount, 100);
+    })
+
+    it('should stablish a second transaction also as pending transaction', ()=>{
+        myNewCoin.createNewTransaction(200, 'ISA1988303DDF', 'SUS8383JDKDHDH');
+        assert.equal(myNewCoin.pendingTransactions.length, 2);
+    })
+    it('should stablish correctly the address of the sender', ()=>{
+        assert.equal(myNewCoin.pendingTransactions[1].sender, 'ISA1988303DDF');
     })
 })
